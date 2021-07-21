bbff56d40f6b19972a7276275b48cb7e94d3cd57
ShenJieSuzhou <745689122@qq.com>
2021-07-21 15:51:29 +0800

    配置完测试所需的内购类型商品。

--- PurchaseXTest/PurchaseXTest/ContentView.swift
+++ PurchaseXTest/PurchaseXTest/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section(header: Text("Producst")) {
+
+                }
+
+                Section(header: Text("VIP Services")) {
+
+                }
+
+                Section(header: Text("Subscriptions")) {
+
+                }
+            }.listStyle(.insetGrouped)
+            .navigationTitle("PurchaseX")
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
-
+                                                               
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        if #available(iOS 15.0, *) {
+            ContentView()
+                .previewInterfaceOrientation(.portraitUpsideDown)
+        } else {
+            // Fallback on earlier versions
+        }
     }
 }
