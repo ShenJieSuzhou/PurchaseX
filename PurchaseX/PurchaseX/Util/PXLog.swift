5f1c8bd091d41faf3c51dca33449732dafb62d32
ShenJieSuzhou <745689122@qq.com>
2021-07-23 15:31:21 +0800

    complete PXLog class。

--- PurchaseX/PurchaseX/Util/PXLog.swift
+++ PurchaseX/PurchaseX/Util/PXLog.swift
@@ -11,9 +11,76 @@ import os.log
 /// we use Apple's unified logging system to log errors, notifications and general messages.
 
 public struct PXLog {
-    private static let iapLog = OSLog(subsystem: Bundle.main.bundleIdentifier, category: "IAP")
+    private static let iapLog = OSLog(subsystem: Bundle.main.bundleIdentifier, category: "StoreKit")
     
+    public static func event(_ event: PurchaseNotification) {
+        #if DEBUG
+        print(event.shortDescription())
+        #else
+        os_log("%{public}s", log: iapLog, type: .default, event.shortDescription())
+        #endif
+    }
+    
+    public static func event(_ event: PurchaseNotification, productId: String) {
+        #if DEBUG
+        print("\(event.shortDescription()) for product \(productID)")
+        #else
+        os_log("%{public}s for product %{public}s", log: iapLog, type: .default, event.shortDescription(), productId)
+        #endif
+    }
+    
+    public static func event(_ event: PurchaseNotification, productId: ProductId, webOrderLineItemId: String?) {
+        #if DEBUG
+        print("\(event.shortDescription()) for product \(productId) with webOrderLineItemId \(webOrderLineItemId ?? "none")")
+        #else
+        os_log("%{public}s for product %{public}s with webOrderLineItemId %{public}s",
+               log: storeLog,
+               type: .default,
+               event.shortDescription(),
+               productId,
+               webOrderLineItemId ?? "none")
+        #endif
+    }
+    
+    public static var transactionLog: Set<TransactionLog> = []
+    
+    public static func transaction(_ event: PurchaseNotification, productId: ProductId) {
+        
+        let t = TransactionLog(notification: event, productId: productId)
+        if transactionLog.contains(t) { return }
+        transactionLog.insert(t)
+        
+        #if DEBUG
+        print("\(event.shortDescription()) for product \(productId)")
+        #else
+        os_log("%{public}s for product %{public}s", log: storeLog, type: .default, event.shortDescription(), productId)
+        #endif
+    }
+    
+    public static func exception(_ exception: StoreException, productId: String) {
+        #if DEBUG
+        print("\(exception.shortDescription()). For product \(productId)")
+        #else
+        os_log("%{public}s for product %{public}s", log: storeLog, type: .default, exception.shortDescription(), productId)
+        #endif
+    }
+    
+    public static func event(_ message: String) {
+        #if DEBUG
+        print(message)
+        #else
+        os_log("%s", log: storeLog, type: .info, message)
+        #endif
+    }
+}
 
+
+public struct TransactionLog: Hashable {
     
+    let notification: PurchaseNotification
+    let productId: String
     
+    public static func == (lhs: TransactionLog, rhs: TransactionLog) -> Bool {
+        return (lhs.productId == rhs.productId) && (lhs.notification == rhs.notification)
+    }
 }
