81e4efb7dffd3373f59bd4b9d91ec0cbe8f3c190
ShenJieSuzhou <745689122@qq.com>
2021-07-23 10:02:52 +0800

    define PurchaseState

---
+++ PurchaseX/PurchaseX/PurchaseHelper/PurchaseState.swift
@@ -0,0 +1,20 @@
+//
+//  PurchaseState.swift
+//  PurchaseX
+//
+//  Created by shenjie on 2021/7/23.
+//
+
+import Foundation
+import SwiftUI
+
+enum PurchaseState {
+    case notStarted
+    case inProgress
+    case complete
+    case pending
+    case cancelld
+    case failed
+    case failedVerification
+    case unknown
+}
