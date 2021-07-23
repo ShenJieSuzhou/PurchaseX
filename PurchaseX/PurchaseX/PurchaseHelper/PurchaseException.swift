c34b0d6272304f8d649a44af80ba38829fdd503a
ShenJieSuzhou <745689122@qq.com>
2021-07-23 14:27:10 +0800

    complete define StoreException class

--- PurchaseX/PurchaseX/PurchaseHelper/PurchaseException.swift
+++ PurchaseX/PurchaseX/PurchaseHelper/PurchaseException.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+public enum StoreException: Error, Equatable {
+    case purchaseException
+    case purchaseInProgressException
+    case transactionVerificationFailed
+    
+    public func shortDescription() -> String {
+        switch self {
+        case .purchaseException:
+            return "Exception: StoreKit throw an exception while processing a purchase"
+        case .purchaseInProgressException:
+            return "Exception: You can't start another purchase yet, one is already in process"
+        case .transactionVerificationFailed:
+            return "Exception: A transaction failed Storekit's verification"
+        }
+    }
+}
