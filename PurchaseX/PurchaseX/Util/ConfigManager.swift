5394e7e584706d045fba927ebf98cf14f9eb2d2c
ShenJieSuzhou <745689122@qq.com>
2021-07-23 16:00:30 +0800

    Complete read configuration file function and define productId  in plist file.

--- PurchaseX/PurchaseX/Util/ConfigManager.swift
+++ PurchaseX/PurchaseX/Util/ConfigManager.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import PurchaseXTest
+
+
+public struct ConfigManager {
+    
+    public static let ConfigFile = "Products"
+    
+    /// read plist file to get products info
+    public static func readConfigFile() -> Set<String> {
+        
+        guard let result = ConfigManager.readPropertyFile(filename: ConfigFile) else {
+            PXLog.event(.configurationNotFound)
+            PXLog.event(.configurationFailure)
+            return nil
+        }
+        
+        guard result.count > 0 else {
+            PXLog.event(.configurationEmpty)
+            PXLog.event(.configurationFailure)
+            return nil
+        }
+        
+        guard let values = result[ConfigFile] as? [String] else {
+            PXLog.event(.configurationEmpty)
+            PXLog.event(.configurationFailure)
+            return nil
+        }
+        
+        return Set<String>(values.compactMap { $0 } )
+    }
+    
+    /// Read plist file and return a dictionary of values
+    private static func readPropertyFile(filename: String) -> [String : AnyObject]? {
+        if let path = Bundle.main.path(forResource: filename, ofType: "plist") {
+            if let contents = NSDictionary(contentsOfFile: path) as? [String : AnyObject] {
+                return contents
+            }
+        }
+        
+        return nil // [:]
+    }
+}
