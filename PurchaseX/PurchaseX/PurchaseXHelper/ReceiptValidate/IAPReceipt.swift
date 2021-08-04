abe07fdbfdccc5d00d790a1e099a2f05a99de9c7
ShenJieSuzhou <745689122@qq.com>
2021-08-04 14:59:46 +0800

    Add IAPReceipt class.

---
+++ PurchaseX/PurchaseX/PurchaseXHelper/ReceiptValidate/IAPReceipt.swift
@@ -0,0 +1,18 @@
+//
+//  IAPReceipt.swift
+//  PurchaseX
+//
+//  Created by shenjie on 2021/8/4.
+//
+
+import Foundation
+
+
+
+/// IAPReceipt is used to verify Appstore purchase receipts locally.
+public class IAPReceipt {
+    
+    
+    
+    
+}
