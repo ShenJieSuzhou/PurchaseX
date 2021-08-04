c54b81f85a41ef2433df348930c87542bc428e62
ShenJieSuzhou <745689122@qq.com>
2021-08-04 18:00:47 +0800

    Add validating Apple Signed the Receipt functions.

--- PurchaseX/PurchaseX/PurchaseXHelper/ReceiptValidate/IAPReceipt+ValidateAppleSigning.swift
+++ PurchaseX/PurchaseX/PurchaseXHelper/ReceiptValidate/IAPReceipt+ValidateAppleSigning.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+/// Validating Apple Signed the Receipt
+extension IAPReceipt {
+    
+    
+    /// Loads Apple’s root certificate from the bundle and converts it to a BIO object.
+    /// Verify a certificate in the chain from the root certificate signed the receipt
+    /// - Returns: Returns true if correctly signed, false otherwise.
+    public func validateSigning() -> Bool {
+        
+        guard receiptData != nil else {
+            PXLog.event("receiptValidateSigningFailure")
+            return false
+        }
+        
+        guard let rootCertificateUrl = Bundle.main.url(forResource: PurchaseXConstants.Certificate(), withExtension: PurchaseXConstants.CertificateExt()), let rootCertificateData = try? Data(contentsOf: rootCertificateUrl) else {
+            PXLog.event("receiptValidateSigningFailure")
+            return false
+        }
+        
+        let rootCertBio = BIO_new(BIO_s_mem())
+        let rootCertBytes: [UInt8] = .init(rootCertificateData)
+        BIO_write(rootCertBio, rootCertBytes, Int32(rootCertificateData.count))
+        let rootCertX509 = d2i_X509_bio(rootCertBio, nil)
+        BIO_free(rootCertBio)
+        
+        let store = X509_STORE_new()
+        X509_STORE_add_cert(store, rootCertX509)
+        
+        OPENSSL_init_crypto(UInt64(OPENSSL_INIT_ADD_ALL_DIGESTS), nil)
+        
+        #if DEBUG
+        let verificationResult = PKCS7_verify(receiptData, nil, store, nil, nil, PKCS7_NOCHAIN)
+        #else
+        let verificationResult = PKCS7_verify(receiptData, nil, store, nil, nil, nil)
+        #endif
+
+        guard verificationResult == 1  else {
+            PXLog.event("receiptValidateSigningFailure")
+            return false
+        }
+
+        PXLog.event("receiptValidateSigningSuccess")
+        
+        return true
+    }
+}
